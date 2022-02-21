78455570c116adec0a2ad8868419e54ee0e1fb8a
Oliver M. Crook <ococrook@gmail.com>
2022-02-21 15:52:24 +0000

    push data and changes

---
+++ R/IonMobility-utils.R
@@ -0,0 +1,11 @@
+##' Utility functions fo Ion Mobility mass spectrometry dat
+##' 
+##' @param object An object of type `Spectra`
+##' @param fcol A character vector indicating the ion mobility dimension column
+##' default is "IonMobilityDriftTime"
+##' @return An estimate of the pusher frequency
+##' @md
+##' 
+##' @rdname IonMobility-util
+##' @example 
+##' 
